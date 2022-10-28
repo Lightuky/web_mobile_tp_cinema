a4dc5395b9a035e34d09e544c37bfdd5d998382d
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-10-28 16:48:15 +0200

    Update movie.service.ts

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -11,12 +11,7 @@ export class MovieService {
   }
 
   public getMoviesByQuery(queryString: string) {
-    return this.http.get<any>(`${env.apiUrl}/search/movie?api_key=${env.apiKey}&language=en_US&query=${queryString}&page=1&include_adult=false`, {
-      'headers': new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'https://ynov-m2-tp-cinema.herokuapp.com/',
-      })
-    });
+    return this.http.get<any>(`${env.apiUrl}/search/movie?api_key=${env.apiKey}&language=en_US&query=${queryString}&page=1&include_adult=false`);
   }
 
 }
